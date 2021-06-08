780cf487a189c7d89d1e9a589a097d3e1dccded5
Billal Fauzan <billal.xcode@gmail.com>
2021-06-09 05:57:04 +0700

    UPDATE: errros exception

--- errors.py
+++ errors.py
@@ -1,3 +1,9 @@
 class ObjectError(Exception):
     def __init__(self, o):
         print (f"Terjadi kesalahan pada objek: {o}")
+
+class DataError(Exception):
+    pass
+
+class FileError(Exception):
+    pass
