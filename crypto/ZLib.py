52ad0a8b9bbdbb890b20f5e60b4bc61f92b71868
Billal Fauzan <billal.xcode@gmail.com>
2021-06-08 17:29:28 +0700

    ADD: ZLib compressor

---
+++ crypto/ZLib.py
@@ -0,0 +1,34 @@
+import random
+import zlib
+from utils import check_byte, check_string
+
+class CompressObjectError(Exception):
+    pass
+
+def generate_keys(length, alp=True, nums=True):
+    ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    NUMBERS = "1234567890"
+    OUTPUTS = ""
+    alp_lists = ""
+    if alp:
+        alp_lists += ALPHABET
+    elif nums:
+        alp_lists += NUMBERS
+    else:
+        alp_lists += ALPHABET
+        alp_lists += NUMBERS
+
+    for _ in range(length):
+        OUTPUTS += random.choice(alp_lists)
+    return OUTPUTS
+
+def compress(data_original):
+    default = 4
+    keys = generate_keys(default)
+    keys = "|" + keys
+    keys = keys.encode()
+    if check_byte(data_original):
+        current_data = zlib.compress(data_original)
+        return current_data + keys
+    else:
+        return False
